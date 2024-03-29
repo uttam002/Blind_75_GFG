855ef28b9d598b140f0627774e8f85f9cab16c81
Uttam Bhuva <112840672+uttam002@users.noreply.github.com>
2024-03-29 15:01:56 +0530

    29 March 2024 - medium level problem

---
+++ Euler Circuit in an Undirected Graph.java
@@ -0,0 +1,9 @@
+class Solution {
+    
+    public boolean isEularCircuitExist(int v, ArrayList<ArrayList<Integer>> adj) {
+        for(int i = 0; i < v; i++){
+            if(adj.get(i).size() % 2 != 0) return false;
+        }
+        return true;
+    }
+}
